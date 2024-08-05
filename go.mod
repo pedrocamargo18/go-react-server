3eb4ccab993b32b70434dd2aa61d2eb2d8506588
Pedro Luz <pedroluz895@gmail.com>
2024-08-05 13:52:30 -0300

    create initial commit to go-react project

---
+++ go.mod
@@ -0,0 +1,6 @@
+module github.com/pedrocamargo18/go-react-server.git
+
+go 1.22.5
+
+
+require github.com/joho/godotenv v1.5.1
---
+++ internal/store/pgstore/migrations/002_create_messages_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXIST messages(
+    "id" uuid PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    "room_id" uuid NOT NULL DEFAULT,
+    "message" VARCHAR(255) NOT NULL,
+    "reaction_count" BIGINT NOT NULL DEFAULT 0,
+    "answered"  BOOLEAN NOT NULL DEFAULT false,
+
+    FOREIGN KEY(room_id) REFERENCES rooms(id)
+);
+---- create above / drop below ----
+DROP TABLE IF EXISTS messages;
